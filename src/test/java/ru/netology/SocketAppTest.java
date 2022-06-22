3ee1790cb9542e4ce23f8bbfbe248e7cc0612266
Mikhail Baranovskii <bm_y@mail.ru>
2022-06-22 11:15:14 +0300

    Initial commit

---
+++ src/test/java/ru/netology/SocketAppTest.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+import static org.junit.Assert.assertTrue;
+
+public class SocketAppTest {
+
+//    @Test
+//    public void shouldAnswerWithTrue() {
+//        assertTrue(true);
+//    }
+}
